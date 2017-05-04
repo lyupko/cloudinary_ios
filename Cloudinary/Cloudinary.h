285d8c99258a900cc071f8f479c354736bb47360
Liubomyr Hlozhyk <lyupko@Liubomyrs-MacBook-Pro.local>
2017-05-04 07:28:52 +0300

    some update

--- Cloudinary/Cloudinary.h
+++ Cloudinary/Cloudinary.h
@@ -2,11 +2,18 @@
 //  Cloudinary.h
 //  Cloudinary
 //
-//  Created by Tal Lev-Ami on 1/11/12.
-//  Copyright (c) 2012 Cloudinary Ltd. All rights reserved.
+//  Created by Liubomyr Hlozhyk on 5/4/17.
+//  Copyright © 2017 Liubomyr Hlozhyk. All rights reserved.
 //
 
-#import "CLCloudinary.h"
-#import "CLTransformation.h"
-#import "CLEagerTransformation.h"
-#import "CLUploader.h"
+#import <UIKit/UIKit.h>
+
+//! Project version number for Cloudinary.
+FOUNDATION_EXPORT double CloudinaryVersionNumber;
+
+//! Project version string for Cloudinary.
+FOUNDATION_EXPORT const unsigned char CloudinaryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Cloudinary/PublicHeader.h>
+
+
